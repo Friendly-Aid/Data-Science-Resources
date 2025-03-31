331dd54de6cc2c1ec9d740f851549a9fc3eebcfe
Friendly-Aid <163163308+Friendly-Aid@users.noreply.github.com>
2025-03-31 13:58:04 -0600

    Made some code, this commit features stock_app.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+
+# Fetch S&P 500 companies (just a sample of popular stocks)
+def get_sp500_tickers():
+    sp500_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    tables = pd.read_html(sp500_url)  # Reads all tables from the Wikipedia page
+    sp500_table = tables[0]  # The first table contains the stock tickers
+    tickers = sp500_table['Symbol'].tolist()  # Extracting the tickers as a list
+    return tickers
+
+# Fetch the list of S&P 500 tickers
+sp500_tickers = get_sp500_tickers()
+print(sp500_tickers[:10])  # Display the first 10 tickers
